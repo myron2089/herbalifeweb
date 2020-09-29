84fa6994d8d953db217f4930a0c285857e15c1f9
myron2089 <myron2089@gmail.com>
2020-09-29 11:14:02 -0600

    herbalife to multicerplus

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,7 +16,7 @@
             <div class="kt-container  kt-container--fluid ">
                 <div class="kt-subheader__main">
                     <h3 class="kt-subheader__title">
-                    Herbalife </h3>
+                    MulticerPlus </h3>
                     <span class="kt-subheader__separator kt-hidden"></span>
                     <div class="kt-subheader__breadcrumbs">
                         <a href="#" class="kt-subheader__breadcrumbs-home"><i class="flaticon2-shelter"></i></a>
